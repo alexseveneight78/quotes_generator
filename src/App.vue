9d5d7919d6a980421f7b5121562cf403c9cf8980
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-07-17 17:00:48 +0300

    Adding New Quote

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="container">
-        <h3>Quotes Generator</h3>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
     export default {
-        
+        data: function(){
+            return {
+                quotes: [
+                    'Just a quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote){
+                this.quotes.push(quote);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
     }
 </script>
 
