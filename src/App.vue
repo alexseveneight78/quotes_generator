6173840d17a67f7570b3376db15196067d1ff7b1
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-07-17 12:30:40 +0300

    In the beginning

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h3>Quotes Generator</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+</style>
